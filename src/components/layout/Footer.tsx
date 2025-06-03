76d5b51e346c3efb58d4746704f0b9bd5568cd5b
MehulPardeshi <135437151+MehulPardeshi@users.noreply.github.com>
2025-06-03 17:21:17 +0530

    Update LinkedIn company URL in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,7 +18,7 @@ const Footer: React.FC = () => {
               Discover, share, and join the best tech events happening in your city. Stay connected with your local tech community and never miss what's happening around you.
             </p>
             <div className="flex space-x-4 mt-2">
-              <a href="https://linkedin.com/company/loopin" className="text-accent-black hover:text-accent-white hover:bg-accent-black p-1 rounded transition-colors duration-200">
+              <a href="https://www.linkedin.com/company/city-loopin/?viewAsMember=true" className="text-accent-black hover:text-accent-white hover:bg-accent-black p-1 rounded transition-colors duration-200">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm15.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z" />
